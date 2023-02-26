e341d16fa797cad7105baddcb674c94b1c389d02
Olaleye Ayomikun <mikunolaleye@gmail.com>
2023-02-26 16:04:55 +0100

    app fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import './App.css';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing/Landing';
 
 
 function App() {
   return (
-    <React.Fragment>
-      <Routes>  
-          <Route index element={<Landing />} />
-      </Routes>
-    </React.Fragment>
+    <Landing />
   );
 }
 
