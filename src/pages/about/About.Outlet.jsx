3baae23bb61e6c6dc80bc006d29e7e8bd6877729
Olaleye Ayomikun <mikunolaleye@gmail.com>
2023-02-26 17:20:31 +0100

    margin fix

--- src/pages/about/About.Outlet.jsx
+++ src/pages/about/About.Outlet.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function AboutOutlet() {
     return (
-        <div name="about" className='bg-[#F5FBFF] w-full h-full text-gray-900'>
+        <div name="about" className='bg-[#F5FBFF] w-full h-full text-gray-900 sm:mt-48'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
