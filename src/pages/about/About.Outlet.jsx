0c9257fffbc64d7de596d845a3b6d4d01fb01a43
Olaleye Ayomikun <mikunolaleye@gmail.com>
2023-02-28 15:09:04 +0100

    text fix

--- src/pages/about/About.Outlet.jsx
+++ src/pages/about/About.Outlet.jsx
@@ -8,7 +8,7 @@ function AboutOutlet() {
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
                 <p className='text-xl mt-4'>
-                    Hi, my name is Festus-Olaleye Ayomikun. A skilled front-end developer who has made significant contributions to web development. With several years of experience, I have become proficient in using HTML, CSS, and JavaScript to create visually appealing and user-friendly interfaces for websites and applications.
+                    Hi, my name is Festus-Olaleye Ayomikun. A skilled front-end developer who has made significant contributions to web development. With 3 years of experience, I have become proficient in using HTML, CSS, and JavaScript to create visually appealing and user-friendly interfaces for websites and applications.
                 </p>
 
                 <br />
